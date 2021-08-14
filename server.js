62c2bbc6e9f1bde383942b20b60b5048ae9cd77e
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-14 12:35:41 -0400

    Refactoring the project to use gatsby

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/express_backend", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
---
+++ story-rpg/gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Chasm",
+    description: "A story website with built-in RPG elements!",
+  },
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
+}
