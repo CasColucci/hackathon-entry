2ed2b6a4ce7a2fe1bb829ec4560902e4e7de085f
Cas <51390039+CasColucci@users.noreply.github.com>
2021-08-14 08:51:46 -0400

    After messing around with libraries, I think I have a good path forward

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
-
-app.listen(3000);
